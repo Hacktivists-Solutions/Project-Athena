88560b2d862f8b8d207a3481189fe0db428771bb
nilay nath sharan <nilaynathsharan16@gmail.com>
2024-03-02 03:54:58 +0530

    feat: intial landing page

---
+++ src/components/hero.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { HeroParallax } from "@/ui/hero-parralax";
+import React from "react";
+
+export function HeroParallaxDemo() {
+  return <HeroParallax products={products} />;
+}
+export const products = [
+  {
+    title: "Hospital Locator",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331399/athena/i5hunengzag0wgm7o3ck.jpg",
+  },
+  {
+    title: "Educatioal Resource",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331391/athena/tzgswdkyeyetgjnq7u8v.jpg",
+  },
+  {
+    title: "Sleep Tracker",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331384/athena/saihjtdcb5r9urvtedvq.jpg",
+  },
+  {
+    title: "Regional Blogs",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331367/athena/xq02zarfscewwvoucdj3.jpg",
+  },
+  {
+    title: "Period Tracker",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331349/athena/jpflrd9fm30enibtw4lb.png",
+  },
+  {
+    title: "SMS Queries",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331469/athena/x5j9hgktvzyuk3abz9ha.jpg",
+  },
+  {
+    title: "Hospital Locator",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331399/athena/i5hunengzag0wgm7o3ck.jpg",
+  },
+  {
+    title: "Educatioal Resource",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331391/athena/tzgswdkyeyetgjnq7u8v.jpg",
+  },
+  {
+    title: "SMS Queries",
+    link: "",
+    thumbnail:
+      "https://res.cloudinary.com/di1h27v1s/image/upload/v1709331469/athena/x5j9hgktvzyuk3abz9ha.jpg",
+  },
+];
