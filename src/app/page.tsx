dca00997dbdd3a7990e0d75049f2de1c8565bcc7
nilay nath sharan <nilaynathsharan16@gmail.com>
2024-03-02 12:21:20 +0530

    feat: intial landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { BackgroundBeamsDemo } from "@/components/ContactUs";
+import { CardHoverEffectDemo } from "@/components/ProductSection";
 import { HeroParallaxDemo } from "@/components/hero";
 import Image from "next/image";
 
@@ -5,6 +7,8 @@ export default function Home() {
   return (
     <>
       <HeroParallaxDemo />
+      <CardHoverEffectDemo />
+      <BackgroundBeamsDemo />
     </>
   );
 }
